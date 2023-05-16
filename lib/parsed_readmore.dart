6503fd6004762cc91a6c21478cc1c6c31517820a
aashish2604 <aashish.260401@gmail.com>
2023-05-16 14:47:50 +0530

    debug: export and unit test

--- lib/parsed_readmore.dart
+++ lib/parsed_readmore.dart
@@ -1,3 +1,4 @@
 library parsed_readmore;
 
 export 'src/parsed_readmore.dart';
+export 'src/target_text_highlight.dart';
--- test/parsed_readmore_test.dart
+++ test/parsed_readmore_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:parsed_readmore/parsed_readmore.dart';
-
 void main() {
   test('adds one to input values', () {});
 }
