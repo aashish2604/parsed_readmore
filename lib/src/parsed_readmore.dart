2125eeeb5bf53af13be99a7eea07cbd91c9f2781
aashish2604 <aashish.260401@gmail.com>
2023-10-22 01:15:51 +0530

    modularised the package class

--- lib/src/parsed_readmore.dart
+++ lib/src/parsed_readmore.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:parsed_readmore/src/parser.dart';
 import 'package:parsed_readmore/src/target_text_highlight.dart';
-import 'package:parsed_readmore/src/util/colors.dart';
-import 'package:parsed_readmore/src/util/regex.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 enum TrimMode {
   length,
@@ -136,177 +134,13 @@ class ParsedReadMoreState extends State<ParsedReadMore> {
       recognizer: TapGestureRecognizer()..onTap = _onTapLink,
     );
 
-    /// Function to get the occurances of string in the spans
-    List<int> findWordOccurrences(String text, String word) {
-      List<int> occurrences = [];
-      int index = 0;
-
-      while (index != -1) {
-        index = text.toLowerCase().indexOf(word.toLowerCase(), index);
-
-        if (index != -1) {
-          occurrences.add(index);
-          index += word.length;
-        }
-      }
-
-      return occurrences;
-    }
-
-    //Text Span detection and highlighting function
-    List<TextSpan> highlightText(List<TextSpan> textSpans) {
-      List<TextSpan> alteredTextSpans = <TextSpan>[];
-      int k = 0;
-      for (int i = 0; i < textSpans.length; i++) {
-        TextSpan span = textSpans[i];
-        String? text = span.text;
-
-        if (text != null && widget.highlightText != null) {
-          if (text.length >= 3) {
-            String? substr = text.substring(0, 3);
-            List<int> firstInd = <int>[];
-            if (substr != '|~|' &&
-                text.contains(widget.highlightText!.targetText)) {
-              firstInd =
-                  findWordOccurrences(text, widget.highlightText!.targetText);
-              if (firstInd[0] > 0) {
-                alteredTextSpans.insert(
-                    k,
-                    TextSpan(
-                        text: text.substring(0, firstInd[0]),
-                        style: effectiveTextStyle));
-              } else {
-                k--;
-              }
-              for (int j = 0; j < firstInd.length; j++) {
-                k++;
-                int s = widget.highlightText!.targetText.length;
-                alteredTextSpans.insert(
-                    k,
-                    TextSpan(
-                        text: text.substring(firstInd[j], firstInd[j] + s),
-                        style: widget.highlightText!.style));
-                k++;
-                if (j == firstInd.length - 1) {
-                  alteredTextSpans.insert(
-                      k,
-                      TextSpan(
-                          text: text.substring(firstInd[j] + s),
-                          style: effectiveTextStyle));
-                } else {
-                  alteredTextSpans.insert(
-                      k,
-                      TextSpan(
-                          text:
-                              text.substring(firstInd[j] + s, firstInd[j + 1]),
-                          style: effectiveTextStyle));
-                }
-              }
-              k++;
-            } else {
-              alteredTextSpans.insert(k, textSpans[i]);
-              k++;
-            }
-          } else {
-            alteredTextSpans.insert(k, textSpans[i]);
-            k++;
-          }
-        }
-      }
-      return alteredTextSpans;
-    }
-
-    ///Function to get the list of substrings on the basis of them being a link or not
-    List<TextSpan> getSentenceList(String s,
-        [int? limit, bool condition = false]) {
-      // int lengthSum = 0;
-      final RegExp exp = RegExp(kUrlRegEx);
-      final Iterable<RegExpMatch> matches = exp.allMatches(s);
-      final Map<int, int> indices = {};
-      for (var match in matches) {
-        indices[match.start] = match.end;
-      }
-
-      final List<String> sentences = [];
-      if (indices.isNotEmpty) {
-        int flag = 0;
-        indices.forEach((key, value) {
-          if (flag != key) sentences.add(s.substring(flag, key));
-          sentences.add("|~|${s.substring(key, value)}");
-          flag = value;
-        });
-        if (flag != s.length) sentences.add(s.substring(flag));
-      } else {
-        sentences.add(s);
-      }
-      List<TextSpan> listOfTextSpans = [];
-      bool loopBreak = false;
-
-      for (String val in sentences) {
-        if (condition && _readMore) {
-          if (widget.trimMode == TrimMode.line && val.length > limit!) {
-            val = val.substring(0, limit);
-            loopBreak = true;
-          }
-
-          if (widget.trimMode == TrimMode.length && val.length > limit!) {
-            val = val.substring(0, limit);
-            loopBreak = true;
-          }
-        }
-        if (loopBreak == false && limit != null) {
-          limit = limit - val.length;
-        }
-
-        String url = '';
-        if (val.length >= 3) {
-          if (val.substring(0, 3) == '|~|') {
-            if (limit != null) {
-              limit = limit + 3;
-            }
-            url = val.substring(3);
-
-            ///TextSpan if the string of the list is a url
-            listOfTextSpans.add(TextSpan(
-                text: url,
-                style: widget.urlTextStyle ??
-                    effectiveTextStyle!.copyWith(
-                        color: kAzureRadianceColor,
-                        decoration: TextDecoration.underline),
-                recognizer: TapGestureRecognizer()
-                  ..onTap = () {
-                    if (!url.startsWith('http://') &&
-                        !url.startsWith('https://')) url = 'https://$url';
-
-                    if (widget.onTapLink != null) {
-                      widget.onTapLink!.call(url);
-                    } else {
-                      try {
-                        Uri launchUri = Uri.parse(url);
-                        launchUrl(launchUri,
-                            mode: LaunchMode.externalApplication);
-                      } on Exception catch (e) {
-                        throw Exception(e);
-                      }
-                    }
-                  }));
-          } else {
-            listOfTextSpans.add(TextSpan(text: val, style: effectiveTextStyle));
-          }
-        } else {
-          listOfTextSpans.add(TextSpan(text: val, style: effectiveTextStyle));
-        }
-        if (loopBreak) {
-          break;
-        }
-      }
-      if (widget.highlightText != null) {
-        var finalSpans = highlightText(listOfTextSpans);
-        return finalSpans;
-      } else {
-        return listOfTextSpans;
-      }
-    }
+    Parser parser = Parser(
+        readMore: _readMore,
+        trimMode: widget.trimMode,
+        urlTextStyle: widget.urlTextStyle,
+        effectiveTextStyle: effectiveTextStyle,
+        onTapLink: widget.onTapLink,
+        highlightText: widget.highlightText);
 
     Widget result = LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
@@ -361,7 +195,7 @@ class ParsedReadMoreState extends State<ParsedReadMore> {
           endIndex = pos.offset;
         }
 
-        ///The code below are the ones which are responsible for the ui of this widget
+        // The code below are the ones which are responsible for the ui of this widget
         InlineSpan textSpan;
         switch (widget.trimMode) {
           case TrimMode.length:
@@ -372,12 +206,13 @@ class ParsedReadMoreState extends State<ParsedReadMore> {
 
               ///Variable created to access the list of the textSpans
               final List<TextSpan> textSpanList =
-                  getSentenceList(widget.data, widget.trimLength, con);
+                  parser.getSentenceList(widget.data, widget.trimLength, con);
               textSpanList.addAll(<TextSpan>[delimiter, link]);
               textSpan = TextSpan(children: textSpanList);
             } else {
               ///Variable created to access the list of the textSpans
-              final List<TextSpan> textSpanList = getSentenceList(widget.data);
+              final List<TextSpan> textSpanList =
+                  parser.getSentenceList(widget.data);
               textSpan = TextSpan(children: textSpanList);
             }
             break;
@@ -387,13 +222,14 @@ class ParsedReadMoreState extends State<ParsedReadMore> {
             ///Condition which determines whether the given text should be trimmed or not
             if (textPainter.didExceedMaxLines) {
               ///Variable created to access the list of the textSpans
-              final List<TextSpan> textSpanList = getSentenceList(
+              final List<TextSpan> textSpanList = parser.getSentenceList(
                   widget.data, endIndex, textPainter.didExceedMaxLines);
               textSpanList.addAll(<TextSpan>[delimiter, link]);
               textSpan = TextSpan(children: textSpanList);
             } else {
               ///Variable created to access the list of the textSpans
-              final List<TextSpan> textSpanList = getSentenceList(widget.data);
+              final List<TextSpan> textSpanList =
+                  parser.getSentenceList(widget.data);
               textSpan = TextSpan(children: textSpanList);
             }
             break;
