6a35bd4dcd8410f49fad23c10d2d34f413e0a356
aashish2604 <aashish.260401@gmail.com>
2023-10-22 01:18:34 +0530

    updated the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,91 +30,147 @@ class ParsedReadMoreDemo extends StatefulWidget {
 }
 
 class _ParsedReadMoreDemoState extends State<ParsedReadMoreDemo> {
-  final TextStyle textStyle = const TextStyle(fontSize: 20);
-  final TextStyle labelTextStyle = const TextStyle(
-      fontSize: 24, color: Colors.grey, fontWeight: FontWeight.bold);
-  static const String inputData =
-      "When using custom values we have specified 'the' to be our target text for highlighting  with purple italic font.\n We know that the website https://google.com is a very useful website. (rti..notNow should not be parsed) But Instagram.com is more fun to use. We should not forget the contribution of wikipedia.com played in the growth of web. If you like this package do consider liking it so that it could be useful to more developers like you. Thank you for your time";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Parsed Read More'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Without using package",
-                style: labelTextStyle,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Parsed Read More'),
+            bottom: const TabBar(tabs: [
+              Tab(
+                text: 'Without',
               ),
-              const SizedBox(
-                height: 10,
+              Tab(
+                text: 'Default',
               ),
-              const Text(inputData),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Divider(
-                  thickness: 3,
-                ),
+              Tab(
+                text: 'Custom',
               ),
-
-              Text(
-                "Using package with default values",
-                style: labelTextStyle,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-
-              //Package widget using only default values.
-              const ParsedReadMore(inputData),
-
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Divider(
-                  thickness: 3,
-                ),
-              ),
-              Text(
-                'Using package with custom values',
-                style: labelTextStyle,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-
-              //Package widget using custom values
-              ParsedReadMore(
-                inputData,
-                urlTextStyle: textStyle.copyWith(
-                    color: Colors.green, decoration: TextDecoration.underline),
-                trimMode: TrimMode.line,
-                trimLines: 4,
-                delimiter: '  ***',
-                onTapLink: (url) {
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text(
-                          "'$url' is displayed because we have used custom onTap function for hyperlinks")));
-                },
-                highlightText: TargetTextHighlight(
-                    targetText: 'the',
-                    style: const TextStyle(
-                        fontSize: 20.0,
-                        fontStyle: FontStyle.italic,
-                        color: Colors.purple)),
-                delimiterStyle: textStyle.copyWith(color: Colors.black),
-                style: textStyle.copyWith(color: Colors.orange),
-                trimCollapsedText: 'expand',
-                trimExpandedText: 'compress',
-                moreStyle: textStyle.copyWith(color: Colors.red),
-                lessStyle: textStyle.copyWith(color: Colors.blue),
-              ),
-            ],
+            ]),
           ),
+          body: const TabBarView(children: [
+            WithoutPackage(),
+            DefaultValuesPackage(),
+            CustomValuesPackage(),
+          ])),
+    );
+  }
+}
+
+class WithoutPackage extends StatelessWidget {
+  const WithoutPackage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const TextStyle labelTextStyle = TextStyle(
+        fontSize: 24, color: Colors.grey, fontWeight: FontWeight.bold);
+    const String inputData =
+        "When using custom values we have specified 'the' to be our target text for highlighting  with purple italic font.\n We know that the website https://google.com is a very useful website. (rti..notNow should not be parsed) But Instagram.com is more fun to use. We should not forget the contribution of wikipedia.com played in the growth of web. If you like this package do consider liking it so that it could be useful to more developers like you. Thank you for your time";
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Without using package",
+              style: labelTextStyle,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(inputData),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DefaultValuesPackage extends StatelessWidget {
+  const DefaultValuesPackage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const TextStyle labelTextStyle = TextStyle(
+        fontSize: 24, color: Colors.grey, fontWeight: FontWeight.bold);
+    const String inputData =
+        "When using custom values we have specified 'the' to be our target text for highlighting  with purple italic font.\n We know that the website https://google.com is a very useful website. (rti..notNow should not be parsed) But Instagram.com is more fun to use. We should not forget the contribution of wikipedia.com played in the growth of web. If you like this package do consider liking it so that it could be useful to more developers like you. Thank you for your time";
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Using package with default values",
+              style: labelTextStyle,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+
+            //Package widget using only default values.
+            ParsedReadMore(inputData),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomValuesPackage extends StatelessWidget {
+  const CustomValuesPackage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const TextStyle textStyle = TextStyle(fontSize: 20);
+    const TextStyle labelTextStyle = TextStyle(
+        fontSize: 24, color: Colors.grey, fontWeight: FontWeight.bold);
+    const String inputData =
+        "When using custom values we have specified 'the' to be our target text for highlighting  with purple italic font.\n We know that the website https://google.com is a very useful website. (rti..notNow should not be parsed) But Instagram.com is more fun to use. We should not forget the contribution of wikipedia.com played in the growth of web. If you like this package do consider liking it so that it could be useful to more developers like you. Thank you for your time";
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Using package with custom values',
+              style: labelTextStyle,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+
+            //Package widget using custom values
+            ParsedReadMore(
+              inputData,
+              urlTextStyle: textStyle.copyWith(
+                  color: Colors.green, decoration: TextDecoration.underline),
+              trimMode: TrimMode.line,
+              trimLines: 4,
+              delimiter: '  ***',
+              onTapLink: (url) {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(
+                        "'$url' is displayed because we have used custom onTap function for hyperlinks")));
+              },
+              highlightText: TargetTextHighlight(
+                  targetText: 'the',
+                  style: const TextStyle(
+                      fontSize: 20.0,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.purple)),
+              delimiterStyle: textStyle.copyWith(color: Colors.black),
+              style: textStyle.copyWith(color: Colors.orange),
+              trimCollapsedText: 'expand',
+              trimExpandedText: 'compress',
+              moreStyle: textStyle.copyWith(color: Colors.red),
+              lessStyle: textStyle.copyWith(color: Colors.blue),
+            ),
+          ],
         ),
       ),
     );
