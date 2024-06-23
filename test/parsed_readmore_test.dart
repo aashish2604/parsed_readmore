ca680fd63b35750fbf9ce3e46212fed3edf3c653
Harshit Kumar <harshit7kr@gmail.com>
2024-06-23 14:50:55 +0530

    adding new tests

--- test/parsed_readmore_test.dart
+++ test/parsed_readmore_test.dart
@@ -1,67 +1,211 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:parsed_readmore/parsed_readmore.dart';
-import 'package:parsed_readmore/src/parser.dart';
 import 'package:parsed_readmore/src/util/colors.dart';
 
 void main() {
-  test('Parser can be created', () {
-    final parser = Parser(
-      readMore: true,
-      trimMode: TrimMode.line,
-      urlTextStyle: const TextStyle(),
-      effectiveTextStyle: const TextStyle(),
-      onTapLink: (url) {},
-    );
-    expect(parser, isNotNull);
-  });
+  const inputText = 'Visit my website at https://example.com';
+  group('TextHighlightParser tests', () {
+    group('Instance tests', () {
+      const parser = TextHighlightParser(data: inputText);
 
-  test('Parser detects links in text', () {
-    final parser = Parser(
-      readMore: true,
-      trimMode: TrimMode.line,
-      urlTextStyle: const TextStyle(),
-      effectiveTextStyle: const TextStyle(),
-      onTapLink: (url) {},
-    );
+      test('TextHighlightParser Instance should be created', () {
+        expect(
+          parser,
+          isNotNull,
+          reason: 'Instance should not be null',
+        );
+      });
 
-    const inputText = 'Visit my website at https://example.com';
-    final result = parser.getSentenceList(inputText);
+      test('TextHighlightParser data should be"$inputText"', () {
+        expect(
+          parser.data,
+          inputText,
+          reason: 'Expected data to be Visit my website at https://example.com',
+        );
+      });
 
-    expect(result, hasLength(2));
-    expect(result[0].text, 'Visit my website at ');
+      test('TextHighlightParser targetTextHighlights should be null', () {
+        expect(
+          parser.targetTextHighlights,
+          isNull,
+          reason: 'Expected targetTextHighlights to be null',
+        );
+      });
+
+      group(
+        'Instance tests with targetTextHighlights',
+        () {
+          final parser = TextHighlightParser(
+            data: inputText,
+            targetTextHighlights: TargetTextHighlights(
+              [
+                const TargetTextHighlight(
+                  targetText: 'Visit',
+                  style: TextStyle(
+                    color: Colors.red,
+                  ),
+                  priority: 1,
+                ),
+                const TargetTextHighlight(
+                  targetText: 'website',
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                  priority: 2,
+                ),
+              ],
+            ),
+          );
+
+          test('TextHighlightParser targetTextHighlights should not be null',
+              () {
+            expect(
+              parser.targetTextHighlights,
+              isNotNull,
+              reason: 'Expected targetTextHighlights to not be null',
+            );
+          });
+
+          test(
+            'TextHighlightParser targetTextHighlights should contain 1 TargetTextHighlight',
+            () {
+              expect(
+                parser.targetTextHighlights?.targetHighlights,
+                hasLength(2),
+              );
+            },
+          );
+
+          test(
+            'getPriorityTargetTextHighlight method test',
+            () {
+              final targetTextHighlight =
+                  parser.targetTextHighlights?.getPriorityTargetTextHighlight();
+
+              expect(
+                targetTextHighlight,
+                isNotNull,
+                reason:
+                    'TargetTextHighlight should not be null. As there are 2 targets.',
+              );
+
+              expect(
+                targetTextHighlight!.priority,
+                2,
+                reason:
+                    'TargetTextHighlight priority should be 2. As this target is with highest priority.',
+              );
+            },
+          );
+
+          group('findUrlRanges method tests', () {
+            final urlRanges = parser.findUrlRanges();
+
+            test('1 Text Range should be created', () {
+              expect(
+                urlRanges,
+                hasLength(1),
+              );
+            });
+            final range = urlRanges.elementAt(0);
+
+            test('Text Range start index should be 20', () {
+              expect(
+                range.start,
+                20,
+                reason: 'Expected start index to be 20',
+              );
+            });
+
+            test('Text Range end index should be 39', () {
+              expect(
+                range.end,
+                39,
+                reason: 'Expected end index to be 39',
+              );
+            });
+
+            test('Text Range text should be" https://example.com"', () {
+              expect(
+                range.textInside(inputText),
+                'https://example.com',
+                reason: 'Expected text inside range to be https://example.com',
+              );
+            });
+
+            test('Text Range text before should be" Visit my website at "', () {
+              expect(
+                range.textBefore(inputText),
+                'Visit my website at ',
+                reason: 'Expected text before range to be Visit my website at ',
+              );
+            });
+
+            test('Text Range text after should be""', () {
+              expect(
+                range.textAfter(inputText),
+                '',
+                reason: 'Expected text after range to be empty',
+              );
+            });
+          });
+        },
+      );
+    });
   });
 
   test('Parser applies default text style to the urls', () {
-    final parser = Parser(
-      readMore: true,
+    const parser = TextHighlightParser(
+      data: inputText,
       trimMode: TrimMode.line,
       urlTextStyle: null,
-      effectiveTextStyle: const TextStyle(),
       onTapLink: null,
     );
+    final allTextHighlights = parser.findTextHighlights();
 
-    const inputText = 'Visit my website at https://example.com';
-    final result = parser.getSentenceList(inputText);
+    final result = parser.getSentenceList(
+      maxShowCharactersLength: parser.data.length,
+      effectiveTextStyle: const TextStyle(),
+      allTextHighlights: parser.findTextHighlights(),
+      allUrlRanges: parser.findUrlRanges(),
+      allIndexTargetTextHighlightsMap:
+          TextHighlightParser.createIndexTargetTextHighlightsMap(
+        allTextHighlights,
+      ),
+      trackActiveTapGesture: (_) {},
+    );
 
     expect(result, hasLength(2));
     expect(
         result[1].style,
         const TextStyle(
-            color: kAzureRadianceColor, decoration: TextDecoration.underline));
+          color: kAzureRadianceColor,
+          decoration: TextDecoration.underline,
+        ));
   });
 
   test('Parser applies custom text style to the urls', () {
-    final parser = Parser(
-      readMore: true,
+    final parser = TextHighlightParser(
+      data: inputText,
       trimMode: TrimMode.line,
       urlTextStyle: const TextStyle(fontSize: 16, color: Colors.green),
-      effectiveTextStyle: const TextStyle(),
       onTapLink: (url) {},
     );
 
-    const inputText = 'Visit my website at https://example.com';
-    final result = parser.getSentenceList(inputText);
+    final allTextHighlights = parser.findTextHighlights();
+
+    final result = parser.getSentenceList(
+      maxShowCharactersLength: parser.data.length,
+      effectiveTextStyle: const TextStyle(),
+      allTextHighlights: parser.findTextHighlights(),
+      allUrlRanges: parser.findUrlRanges(),
+      allIndexTargetTextHighlightsMap:
+          TextHighlightParser.createIndexTargetTextHighlightsMap(
+        allTextHighlights,
+      ),
+      trackActiveTapGesture: (_) {},
+    );
 
     expect(result, hasLength(2));
     expect(result[0].text, 'Visit my website at ');
@@ -69,35 +213,62 @@ void main() {
   });
 
   test('Parser handles non-link text', () {
-    final parser = Parser(
-      readMore: true,
+    const inputText = 'This is a regular text.';
+    final parser = TextHighlightParser(
+      data: inputText,
       trimMode: TrimMode.line,
       urlTextStyle: const TextStyle(),
-      effectiveTextStyle: const TextStyle(),
       onTapLink: (url) {},
     );
 
-    const inputText = 'This is a regular text.';
-    final result = parser.getSentenceList(inputText);
+    final allTextHighlights = parser.findTextHighlights();
+
+    final result = parser.getSentenceList(
+      maxShowCharactersLength: parser.data.length,
+      effectiveTextStyle: const TextStyle(),
+      allTextHighlights: parser.findTextHighlights(),
+      allUrlRanges: parser.findUrlRanges(),
+      allIndexTargetTextHighlightsMap:
+          TextHighlightParser.createIndexTargetTextHighlightsMap(
+        allTextHighlights,
+      ),
+      trackActiveTapGesture: (_) {},
+    );
 
     expect(result, hasLength(1));
     expect(result[0].text, inputText);
   });
 
   test('Parser detects and highlights custom target text', () {
-    final parser = Parser(
-      readMore: true,
+    
+    const inputText = 'Visit my example website at a website.com';
+    final parser = TextHighlightParser(
+      data: inputText,
       trimMode: TrimMode.line,
       urlTextStyle: const TextStyle(),
-      effectiveTextStyle: const TextStyle(),
       onTapLink: (url) {},
-      highlights: [TargetTextHighlight(
-          targetText: 'example', style: const TextStyle(color: Colors.red))],
+      targetTextHighlights: TargetTextHighlights([
+        const TargetTextHighlight(
+          targetText: 'example',
+          priority: 1,
+          style: TextStyle(color: Colors.red),
+        )
+      ]),
     );
 
-    const inputText = 'Visit my example website at a website.com';
-    final result = parser.getSentenceList(inputText);
+    final allTextHighlights = parser.findTextHighlights();
 
+    final result = parser.getSentenceList(
+      maxShowCharactersLength: parser.data.length,
+      effectiveTextStyle: const TextStyle(),
+      allTextHighlights: parser.findTextHighlights(),
+      allUrlRanges: parser.findUrlRanges(),
+      allIndexTargetTextHighlightsMap:
+          TextHighlightParser.createIndexTargetTextHighlightsMap(
+        allTextHighlights,
+      ),
+      trackActiveTapGesture: (_) {},
+    );
     expect(result, hasLength(4));
     expect(result[1].text, 'example');
     expect(result[1].style?.color, Colors.red);
